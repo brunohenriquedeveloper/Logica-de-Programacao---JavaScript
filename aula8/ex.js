e2f700756fbc66e55e054c57903f4e80ce15da2c
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-05-25 19:40:45 -0300

    full application

---
+++ aula8/ex.js
@@ -0,0 +1,4 @@
+let name = "Bruno"
+let idade = 17
+
+console.log("Meu nome é " + name + " eu tenho " + idade + " anos.")
---
+++ aula8/novo.js
@@ -0,0 +1,6 @@
+let n1 = 1
+let n2 = 2
+
+let ope = n1 + n2
+
+console.log(ope)
