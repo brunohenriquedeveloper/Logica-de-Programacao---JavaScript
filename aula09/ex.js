2269ee581e1b5ae42208a34113dd41790a567621
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-05-26 21:12:36 -0300

    full application

---
+++ aula09/ex.js
@@ -0,0 +1,9 @@
+let n1 = window.prompt("digite um número")
+
+n1 = parseFloat(n1)
+
+let n2 = window.prompt("digite mais um número")
+
+n2 = parseFloat(n2)
+
+window.alert("A subtração entre os números é: " + (n1 - n2))
