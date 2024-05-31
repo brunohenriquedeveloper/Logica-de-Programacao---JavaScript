9f8e18e87b5fd1a8df5287f7a4e41f10879cb952
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-05-31 20:07:17 -0300

    Create challlenge.js

---
+++ aula15/challlenge.js
@@ -0,0 +1,35 @@
+const readline = require("readline-sync")
+
+function Bmi(m, h){
+   return (m / h ** 2).toFixed(2)
+}
+
+let name1 = readline.question("What´s your name? ")
+
+let mass1 = readline.question("What´s your weight? ")
+
+let height1 = readline.question("What´s your height? ")
+
+let bmi1 = Bmi(mass1, height1)
+
+console.log (name1+ ": " + bmi1)
+
+let name2 = readline.question("What´s your name? ")
+
+let mass2 = readline.question("What´s your weight? ")
+
+let height2 = readline.question("What´s your height? ")
+
+let bmi2 = Bmi(mass2, height2)
+
+console.log (name2 + ": " + bmi2)
+
+if(parseFloat(bmi1) > parseFloat(bmi2)) {
+    console.log(`${bmi1} > ${bmi2}`)
+    console.log(true)
+} else if (parseFloat(bmi1) = parseFloat(bmi2)) {   console.log(`${bmi1} = ${bmi2}`)
+    console.log("its the same BMI")
+} else {
+    console.log(`${bmi1} < ${bmi2}`)
+    console.log(false)
+}
