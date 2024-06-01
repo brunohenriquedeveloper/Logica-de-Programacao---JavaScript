599934fbc9e45b40f3af1be2af553b7b81236d96
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-06-01 13:31:29 -0300

    Create bill.js

---
+++ aula15/bill.js
@@ -0,0 +1,8 @@
+let bill = 60
+let tip = 0
+
+bill >= 50 && bill <= 300 ? tip = (bill * 0.15) : tip = (bill * 0.20 )
+
+let finalv = bill + tip
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the final value is ${finalv}`)
