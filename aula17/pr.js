67bf710b5dd40f6930c8a1d90cd750ce1b5bf364
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-06-04 20:29:40 -0300

    Update pr.js

--- aula17/pr.js
+++ aula17/pr.js
@@ -1,3 +1,18 @@
 "strict mode";
 
-const x = 23;
+const T = [17, 21, 23]
+
+function printForecast(){
+
+    let days = 1
+    for(let c = 0; c < T.length; c++){
+     
+         console.log(`${T[c]} ºC in ${days} days, `)
+       days = days + 1
+         
+    }
+    
+  
+}
+
+printForecast(T)
