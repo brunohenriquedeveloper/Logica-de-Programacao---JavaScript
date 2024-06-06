c0fe5711acd0d65feb863a47d690ab2665481a43
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-06-05 22:29:27 -0300

    Update compare.js

--- aula19/compare.js
+++ aula19/compare.js
@@ -1,28 +1,31 @@
-const arr = [2, 5, 23, 78, 97, 56, 34, 25]
+const arr = [2, 5, 23, 78, 97, 56, 34, 25];
 
-let old = 0
-let young = 0
+let old = arr[0];
+let young = arr[0];
 
-    function comparisonpo(arr){
-        for(c =1; c< arr.length; c++){
-            if(arr[c] > arr[c-1]){
-                old = arr[c]
-            }
+    function complus(arr){
+        for(c = 1; c <= arr.length; c++){
+           if(arr[c] < arr[c-1]){
+            old = arr[c]
+           }
             
         }
-    }
+}
 
-    function comparisonmin(arr){
-        for(c = arr.length -1; c< arr.length; c--){
-            if(arr[c] < arr[c -1]){
-                young = arr[c]
-                
-            }
-        }
+function cominus(arr){
+    for(c = 1; c < arr.length; c++){
+        if(arr[c] > arr[c-1]){
+            old = arr[c]
+           }
     }
+}
 
-    comparisonpo(arr)
-    comparisonmin(arr)
-    const difer = old - young
-    console.log(difer)
+complus(arr)
+cominus(arr)
+
+const res = old - young
+
+console.log(old)
+console.log(young)
+console.log(res)
    
