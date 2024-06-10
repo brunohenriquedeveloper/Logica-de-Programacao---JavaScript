1bbff5d84ba5e3c7cf6e5c5dbec8bf6d1cb6aa79
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-06-09 21:32:58 -0300

    Update game.js

--- aula18/game.js
+++ aula18/game.js
@@ -8,7 +8,7 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score'),textContent = 10;
 
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value)
 
 */
 
