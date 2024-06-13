b5b99f48dc939263faf5a61dcc8d9ccec2309614
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-06-13 20:30:56 -0300

    Create primo.js

---
+++ aula19/primo.js
@@ -0,0 +1,14 @@
+'strict mode';
+let numbers = [12, 13, 17, 7, 89, 119, 65];
+
+function fat (){
+    let n1 = 2 ;
+
+    for(i=0; i <= n1; i++){
+    if(numbers[i] % n1=== 0){
+
+       
+    } else{
+    n1++
+}
+} }
