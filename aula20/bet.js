dd8416561dbdce31931347ee4504c808e6f98dfc
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-06-18 20:59:38 -0300

    Create bet.js

---
+++ aula20/bet.js
@@ -0,0 +1,99 @@
+const borussia = [players1]
+
+const bayern = [players2]
+
+let players1 = ['Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+]
+
+gk1 = 'Burki'
+
+let fieldplayers1 = ['Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',]
+
+let players2 = [ 'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnabry',
+    'Lewandowski'
+ ]
+
+gk2 = 'Neuer'
+
+let fieldplayers2 = [
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnabry',
+    'Lewandowski'
+]
+
+let allplayers = ['Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnabry',
+    'Lewandowski',]
+
+    const playersFinal = [
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnabry',
+    'Lewandowski',
+    'Coutinho',
+    'Perisic',
+    'Thiago',
+    ]
+
+    
